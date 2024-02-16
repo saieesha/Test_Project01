20ed5964423e494a36265bf27b31514140e484ac
saieesha <48181375+saieesha@users.noreply.github.com>
2024-02-15 19:07:22 -0800

    Update HelloWorld.java

--- src/main/java/com/microserviceslab/graphql/service/HelloWorld.java
+++ src/main/java/com/microserviceslab/graphql/service/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.microserviceslab.graphql.service;
 
 public class HelloWorld {
-    System.out.println("Hello World");
+    System.out.println("Tutorial1");
     System.out.println("Hello World");
 }
